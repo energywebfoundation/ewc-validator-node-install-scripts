d300dfccf648a5698225478c0f724b57fddea374
Adam Z. Nagy <ngy.am.zs@gmail.com>
2020-02-12 11:27:10 +0100

    Update install-validator-ubuntu-server-18.04-volta.sh
    parity checksum

--- volta-affiliate/install-validator-ubuntu-server-18.04-volta.sh
+++ volta-affiliate/install-validator-ubuntu-server-18.04-volta.sh
@@ -7,7 +7,7 @@ DEBIAN_FRONTEND=noninteractive
 
 # Configuration Block - Docker checksums are the image Id
 PARITY_VERSION="parity/parity:v2.5.13-stable"
-PARITY_CHKSUM="sha256:c0083f652c30488e7fde8a44bbc80cad042b36a78bd87ba8947ab24183c659ad"
+PARITY_CHKSUM="sha256:36be05aeb6426b5615e2d6b71c9590dbc4a4d03ae7bcfa53edefdaeef28d3f41"
 
 NODECONTROL_VERSION="v1.0.0"
 NODECONTROL_CHKSUM="sha256:c23d3f66f1c7861c43ba1fe900eb734bcab7e2352f34b51db91beb6f3d757c35"
